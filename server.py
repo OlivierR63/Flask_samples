5e4f6bf29fad91374d0b4b6d1175b66b80645991
OlivierR63 <olivier.rochat@gmail.com>
2025-07-07 13:42:02 -0400

    Fixed typos upon the linting stage

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 # Import the Flask class from the flask module
 from flask import Flask, make_response, request
 
-# Create an instance of the Flask class, passing in the name of the current module
+# Create an instance of the Flask class, passing in the 
+# name of the current module
 app = Flask(__name__)
 
 
@@ -63,16 +64,19 @@ data = [
     }
 ]
 
+
 # Define a route for the route URL ("/")
 @app.route("/")
 def index():
-     # Function that handles request to the root URL
-     # Return a plain text response
-     return "Hello World\n"
+    # Function that handles request to the root URL
+    # Return a plain text response
+    return "Hello World\n"
+
 
 @app.route("/no_content")
 def no_content():
     return ({"message": "No content found"}, 204)
+
 
 @app.route("/exp")
 def index_explicit():
@@ -81,28 +85,32 @@ def index_explicit():
 
         Returns :
             message : Hello World
-            status code : 200 
+            status code : 200
     """
     resp = make_response({"message": "Hello World"})
     resp.status_code = 200
 
     return resp
 
+
 @app.route("/data")
 def get_data():
     try:
         # Check if 'data' exists and has a length greater than 0
         if (data) and len(data) > 0:
-            # Return a JSON response with amessage indicating the length of the data
+            # Return a JSON response with amessage indicating 
+            # the length of the data
             return {"message": f"Data of length {len(data)} found"}
         else:
-            # If data is empty, return a JSON response with a 500 Internal Server Error status code
+            # If data is empty, return a JSON response with a 
+            # "500 Internal Server Error" status code
             return {"message": "Data is empty"}, 500
 
     except NameError:
         # Handle the case where 'data' is not defined
         # Return a JSON response with a 404 Not Found status code
         return {"message": "Data not found"}, 404
+
 
 @app.route("/name_search")
 def name_search():
@@ -121,19 +129,24 @@ def name_search():
     # Check if the query parameter 'q' is missing:
     if query is None:
         return {"message": "Query parameter 'q' is missing"}, 400
-    
-    # Check if the query parameter 'q' is present but invalid (e.g empty or numeric)
+
+    # Check if the query parameter 'q' is present but invalid 
+    # (e.g empty or numeric)
     if (query.strip() == "" or query.isdigit()):
         return {"message": "Invalid input parameter"}, 422
-    
-    # Iterate through the 'data' list to look for the person whose first name matchesthe query
+
+    # Iterate through the 'data' list to look for the person whose first name 
+    # matches the query
     for person in data:
         if query.lower() in person["first_name"].lower():
-            # If a match is found, return the person as a JSON response with a 200 OK status code
+            # If a match is found, return the person as a JSON response 
+            # with a 200 OK status code
             return person, 200
 
-    # If no match is found, return a JSON response with a message indicating the person was not found and a 404 Not Found satus code
+    # If no match is found, return a JSON response with a message indicating 
+    # the person was not found and a 404 Not Found satus code
     return {"message": "Person not found"}, 404
+
 
 @app.get("/count")
 def count():
@@ -143,20 +156,24 @@ def count():
 
     except NameError:
         # Handle the case where 'data' is not defined
-        # Return a JSON response with a message and a 500 Internal Server Error statis code
+        # Return a JSON response with a message and 
+        # a 500 "Internal Server Error" status code
         return {"message": "data not defined"}, 500
-    
+
+
 @app.get("/person/<var_name>")
 def find_by_uuid(var_name):
-    # Iterates throgh the 'data' list  to search for a person with a matching ID
+    # Iterates through the 'data' list to search for a person with a matching ID
     for person in data:
         # Check if the 'id' field of the person matches th 'var_name' parameter
         if person["id"] == str(var_name):
             # Return the person as a JSON response if a match is found
             return person
 
-    # Return a JSON response with a message and a '404 Not Found' status code if no matching person is found
+    # Return a JSON response with a message and a '404 Not Found' status code 
+    # if no matching person is found
     return {"message": "Person Not Found"}, 404
+
 
 @app.route("/person/<var_name>", methods=['DELETE'])
 def delete_by_uuid(var_name):
@@ -164,12 +181,14 @@ def delete_by_uuid(var_name):
         # Remove the person from the data list
         if person["id"] == str(var_name):
             data.remove(person)
-            return {"message":f"Person with ID {var_name} deleted"}, 200
-    
-    # if no perrson with the given ID is found, return a JSON response with a message and HTTP status code 404 (Not Found)
+            return {"message": f"Person with ID {var_name} deleted"}, 200
+
+    # if no perrson with the given ID is found, return a JSON response 
+    # with a message and HTTP status code 404 (Not Found)
     return {"message": "Person Not Found"}, 404
 
-@app.post("/person") # Other solution : @app.route("/person", methods=[POST])
+
+@app.post("/person")  # Other solution : @app.route("/person", methods=[POST])
 def add_by_uuid():
     # Get the JSON data from the incoming request
     new_person = request.get_json()
@@ -185,20 +204,24 @@ def add_by_uuid():
         except NameError:
             return {"message": "data not defined"}, 500
 
-    # Assuming the processing is succesful, return a success message with status code 200 (cerated)
+    # Assuming the processing is successful, return a success message 
+    # with status code 200 (created)
     return {"message": f"New person {new_person['id']} created succesfully"}, 200
+
 
 @app.errorhandler(404)
 def api_not_found(error):
     return {"message": "API not found"}, 404
 
-# This is a global error handler in Flask for any unhandled exceptios
+
+# This is a global error handler in Flask for any unhandled exceptions
 @app.errorhandler(Exception)
-def api_not_found(e):
+def api_not_found2(e):
     return {"message": str(e)}, 500
 
+
 # Test route for testing the global error handler. 
-# Works with the curl command : 'curl http://localhost:5000/test500'
+# Works with the curl command: 'curl http://localhost:5000/test500'
 @app.route("/test500")
 def test500():
     raise Exception("Forced exception for testing")
